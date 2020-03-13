fd36a31e20febcb4885e070aa7bcb22a4444f0f8
Logan Van Hook <loganvan@att.net>
2020-03-13 11:53:11 -0400

    Projects Route

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const server = express();
 
 server.use(express.json());
 
+const ProjectRoute = require("./routes/projects");
+server.use("/projects", ProjectRoute);
+
 server.get("/", (req, res)=>
 {
     res.status(200).send("I'm alive.");
